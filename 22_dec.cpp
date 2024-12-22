c715536211b9b9beb9e51486ae4676a707db1463
Mansi5164 <156164354+Mansi5164@users.noreply.github.com>
2024-12-22 11:23:04 +0530

    search in 2d matrix

---
+++ 22_dec.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool matSearch(vector<vector<int>> &mat, int x) {
+        int row =mat.size();
+        int col = mat[0].size();
+        int rowIndex = 0;
+        int colIndex = col - 1;
+        
+        while(rowIndex < row && colIndex>= 0){
+            int element = mat[rowIndex][colIndex];
+            if(element == x) return true;
+            else if(element < x) rowIndex++;
+            else colIndex--;
+        }
+        return false;
+    }
+int main()
+{
+    int n;
+    int m;
+    cout<<"enter number of rows and columns : ";
+    cin >> n >> m;
+
+    vector<vector<int>> arr(n,vector<int>(m));
+
+    cout <<"enter all the elements of an array :"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int x ;
+    cout <<"enter element to be found:";
+    cin >>x;
+
+    bool flag= matSearch(arr,x);
+    if(flag) cout<<"true"<<endl;
+    else cout<<"false"<<endl;
+
+    return 0;
+}
+
+
+/*
+
+INPUT:
+enter number of rows and columns : 1 6
+enter all the elements of an array :
+18 21 27 38 55 67
+enter element to be found:55
+
+OUTPUT:
+true
+
+*/
